ad46ea4335323501e9443088b10748ece8ed7159
Shahin <shahin.shdm@gmail.com>
2026-05-30 16:44:24 -0700

    fix route for ghpages only

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Doost/',
+//  Change this line to an arrow function that destructures 'mode'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'ghpages' ? '/Doost/' : './'
+  }
 })
